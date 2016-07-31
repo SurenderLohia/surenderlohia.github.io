837606d91570b771ea30c6f5623252a2f9efa072
SurenderLohia <surender.lohia.e@gmail.com>
2016-07-31 13:55:05 +0530

    Lightbox implementation...

--- asserts/js/app/main.js
+++ asserts/js/app/main.js
@@ -2,6 +2,7 @@
 
   $(document).ready(function() {
     slickInit();
+    lightBoxInit();
   });
 
 
@@ -11,6 +12,32 @@
       prevArrow: '<div class="icon a p-left p-v-center trans slider-btn"><i class="fa fa-chevron-left"></i></div>',
       nextArrow: '<div class="icon a p-right p-v-center trans slider-btn"><i class="fa fa-chevron-right"></i></div>'
     });
+  }
+
+  // Light box
+  function lightBoxInit() {
+    $('.light-box-btn').on('click', showLightBox);
+    $('.light-box-close-btn').on('click', closeLightBox);
+
+    function showLightBox(e) {
+      var $overlay = $('.overlay');
+      var currentId = $(e.target).attr('data-href');
+      var $lightBox = $('#' + currentId);
+      $(window).scrollTop(0);
+      $('body').addClass('overflow-hidden');
+
+      $overlay.show();
+      $lightBox.show();
+    }
+
+    function closeLightBox(e) {
+      var $overlay = $('.overlay');
+      var $lightBox = $(e.target).closest('.light-box');
+      $('body').removeClass('overflow-hidden');
+
+      $overlay.hide();
+      $lightBox.hide();
+    }
   }
 
 })(window, document, window.jQuery);
