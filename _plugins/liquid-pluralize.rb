f45e7d7a0e53966c66c4ddf23aabc4751bca8c3a
SurenderLohia <surender.lohia.e@gmail.com>
2016-10-09 19:34:09 +0530

    1. Projects sorting. 2. Create includes for reusabel html 3. Projects design updates.

---
+++ _plugins/liquid-pluralize.rb
@@ -0,0 +1,21 @@
+# pluralize
+#
+# A Liquid filter to make it easy to form correct plurals.
+#
+# https://github.com/bdesham/pluralize
+module Jekyll
+  module Pluralize
+    def pluralize(number, singular, plural = nil)
+      number = Integer(number) || 0
+      if number == 1
+        "#{singular}"
+      elsif plural.nil?
+        "#{singular}s"
+      else
+        "#{plural}"
+      end
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::Pluralize)
